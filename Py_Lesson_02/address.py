5fe94303cb09a58816db7ffd3e4a6e806e1e63d9
ericsilb <eric@silbermans.net>
2016-09-13 10:22:04 -0700

    Added Lesson_02

---
+++ Py_Lesson_02/address.py
@@ -0,0 +1,10 @@
+Name = "Bob Smith"
+City = "San Diego,"
+State = " CA"
+Zip   = " 10000"
+Address = "10000 stinridge Dr."
+
+print(Name)
+print(Address)
+print(City + State + Zip)
+
---
+++ Py_Lesson_02/surfacearea.py
@@ -0,0 +1,5 @@
+Length = 10
+Height = 12
+Width = 10
+surfaceArea = 2 * (Width*Height*Height*Length+Width*Length)
+print(" The surface area of the cube is" , surfaceArea)
