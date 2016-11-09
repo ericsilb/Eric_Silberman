fe8888c05763fa5ee0f9a62cef27a8ca49b9c775
ericsilb <eric@silbermans.net>
2016-11-09 10:16:08 -0800

    e

---
+++ Py_lesson_06/song.py
@@ -0,0 +1,11 @@
+def printLine(word,num):
+    line = ""
+    for i in range(num):
+        line += word + " "
+    print(line)
+
+printLine("Na",4)
+printLine("Na",4)
+printLine("Hey",3)
+printLine("Goodbye!",1)
+
